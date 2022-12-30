776c9367dfd6e1121b2e88ca2ec21fc0b0ca1141
PranitRohokale <pranitrohokale@gmail.com>
2022-12-30 20:05:19 +0530

    Todo Form Functionality done!!

--- src/Components/TodoForm.jsx
+++ src/Components/TodoForm.jsx
@@ -1,5 +1,52 @@
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { database } from "../Appwrite/AppwriteConfig";
+import { TODO_COLLECTION_ID, DATABASE_ID } from "../constants";
+
 const TodoForm = () => {
-  return <div>TodoForm</div>;
+  const [todo, setTodo] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!todo) return;
+    try {
+      const promise = database.createDocument(
+        DATABASE_ID,
+        TODO_COLLECTION_ID,
+        uuidv4(),
+        {
+          todo: todo,
+        }
+      );
+      promise.then((res) => console.log("Added")).finally(() => setTodo(""));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="max-w-7xl mx-auto mt-10">
+      <form
+        action=""
+        onSubmit={handleSubmit}
+        className="flex justify-center mb-10"
+      >
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Enter Todo"
+          className="border p-2 w-2/3 rounded-md"
+          onChange={(e) => setTodo(e.target.value.trim())}
+        />
+        <button
+          className="bg-purple-500 p-2 text-white ml-2 rounded-md"
+          type="submit"
+        >
+          Add Todo
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default TodoForm;
---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const DATABASE_ID = "63aed0cf48baf8d3c022"
+
+//collections
+export const TODO_COLLECTION_ID = "63aef3d4529341122650"
